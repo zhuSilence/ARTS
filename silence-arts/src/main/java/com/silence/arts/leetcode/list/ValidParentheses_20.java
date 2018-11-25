5d6ac5fc894c765ad782455fccafc09b6e82593c
zhuxiang <zhuxiang134@gmail.com>
2018-11-25 20:18:52 +0800

    4 median

--- silence-arts/src/main/java/com/silence/arts/leetcode/list/ValidParentheses_20.java
+++ silence-arts/src/main/java/com/silence/arts/leetcode/list/ValidParentheses_20.java
@@ -26,7 +26,8 @@ public class ValidParentheses_20 {
             Stack<String> stack = new Stack<>();
             for (int i = 0; i < s.length(); i++) {
                 String val = String.valueOf(s.charAt(i));
-                if (!map.containsKey(val)) {
+                //if (!map.containsKey(val)) {
+                if (val.equals("(") || val.equals("[") || val.equals("{")) {
                     stack.push(val);
                 } else if (stack.size() <= 0 || !map.get(val).equals(stack.pop())) {
                     return false;
