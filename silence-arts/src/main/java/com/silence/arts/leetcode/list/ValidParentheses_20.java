22df7a6b97cb6a452643a69a85e0ade69e373e9f
zhuxiang <zhuxiang134@gmail.com>
2018-11-25 17:26:50 +0800

    堆栈 判断括号合法性 20

---
+++ silence-arts/src/main/java/com/silence/arts/leetcode/list/ValidParentheses_20.java
@@ -0,0 +1,44 @@
+package com.silence.arts.leetcode.list;
+
+
+import java.util.*;
+
+/**
+ * <br>
+ * <b>Function：</b><br>
+ * <b>Author：</b>@author Silence<br>
+ * <b>Date：</b>2018-11-25 16:49<br>
+ * <b>Desc：</b>无<br>
+ */
+public class ValidParentheses_20 {
+
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+        if (null != s && s.length() > 0) {
+            Map<String, String> map = new HashMap<>();
+            map.put(")", "(");
+            map.put("]", "[");
+            map.put("}", "{");
+            Stack<String> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                String val = String.valueOf(s.charAt(i));
+                if (val.equals("(") || val.equals("[") || val.equals("{")) {
+                    stack.push(val);
+                } else if (stack.size() <= 0 || !map.get(val).equals(stack.pop())) {
+                    return false;
+                }
+            }
+            //stack needs empty
+            return stack.size() <= 0;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid(""));
+    }
+}
